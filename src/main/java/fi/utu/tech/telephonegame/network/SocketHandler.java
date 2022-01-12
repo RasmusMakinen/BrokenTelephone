5e6f9901edfa9fff770f6790352da18ec25780f2
Veeti Koivuniemi <vehkoi@utu.fi>
2022-01-12 22:29:45 +0000

    networkService.getOutputQueue().add(new Envelope(((Envelope)sisaanTulo.readObject()).getPayload())); lisäys

--- src/main/java/fi/utu/tech/telephonegame/network/SocketHandler.java
+++ src/main/java/fi/utu/tech/telephonegame/network/SocketHandler.java
@@ -9,33 +9,44 @@ public class SocketHandler extends Thread {
 	private ObjectOutputStream ulosTulo;
 	private ObjectInputStream sisaanTulo;
 	private NetworkService networkService;
-	
-	public SocketHandler(Socket socket, NetworkService networkService ) {
+
+	public SocketHandler(Socket socket, NetworkService networkService) {
 		this.socket = socket;
 		this.networkService = networkService;
-		networkService.lisääSocketListiin(this);
-		
+		networkService.lisaaSocketListiin(this);
+
 		//TODO tähän pitäs lisätä 
-		
+
 	}
 
 	public void run() {
 		try {
 			InputStream iS = socket.getInputStream();
-			OutputStream oS = socket.getOutputStream(); 
-			ulosTulo = new ObjectOutputStream(oS); 
+			OutputStream oS = socket.getOutputStream();
+			ulosTulo = new ObjectOutputStream(oS);
 			sisaanTulo = new ObjectInputStream(iS);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	public void send(Envelope env) {
-		try {
-			ulosTulo.writeObject(env);
-			ulosTulo.flush();
-		} catch (IOException e) {
-			e.printStackTrace();
+		while (true) {
+			try {
+				networkService.getOutputQueue().add(new Envelope(((Envelope)sisaanTulo.readObject()).getPayload()));
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+
 		}
 	}
-}
+
+		public void send (Envelope env){
+			try {
+				ulosTulo.writeObject(env);
+				ulosTulo.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
